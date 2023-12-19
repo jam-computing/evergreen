7623cfe1b88f601d668d90d702cb3431643fdf21
Jules White <jules.white1@protonmail.com>
2023-12-19 23:12:53 +0000

    Testing Yew Properties

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use yew::prelude::*;
+use yew::{virtual_dom::VNode, Properties, function_component, html, Html};
 
 fn main() {
     yew::Renderer::<App>::new().render();
@@ -6,19 +6,30 @@ fn main() {
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
     html! {
+        <HelloWorld>
+            <span>{"Hello Evergreen!"}</span>
+            <h1>{"The Sky"}</h1>
+        </HelloWorld>
+    }
+}
+
+// VNode is just alias for Html
+// If function component then practically compiles to <HelloWorld>
+#[function_component]
+fn HelloWorld(props: &Props) -> VNode {
+    // props.on_name_entry.emit(String::from("Matthew"));
+    html! { 
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+            { props.children.clone() }
         </div>
     }
 }
+
+// Must derive properties in order to use struct as parameter
+// The name "children" is really important 
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub children: Html
+}
+
--- target/debug/deps/evergreen-8e14c910a8a76b8e.d
+++ target/debug/deps/evergreen-8e14c910a8a76b8e.d
@@ -1,5 +1,5 @@
-/home/jules/Evergreen/evergreen/target/debug/deps/libevergreen-8e14c910a8a76b8e.rmeta: src/main.rs
+/home/jules/Evergreen/target/debug/deps/libevergreen-8e14c910a8a76b8e.rmeta: src/main.rs
 
-/home/jules/Evergreen/evergreen/target/debug/deps/evergreen-8e14c910a8a76b8e.d: src/main.rs
+/home/jules/Evergreen/target/debug/deps/evergreen-8e14c910a8a76b8e.d: src/main.rs
 
 src/main.rs:
