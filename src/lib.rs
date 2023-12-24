34e3515ed2bf2db1518c94891d0caf080ef6cb73
Jules White <jules.white1@protonmail.com>
2023-12-24 23:51:06 +0000

    Add Animation struct and implementation methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,12 @@
 use std::fs;
 use serde::{ Serialize, Deserialize };
+
 // I have no idea where this code should go?
 // src/tree maybe?
 
-
+// where config is
+// also if we are going to use config files, we should defintely just use one
 pub const PATH: &str = "tree.json";    
-
-pub fn get_tree() -> Option<Tree> {
-    let file = fs::read_to_string(PATH).unwrap();
-    if file == "" {
-        return None
-    }
-    let out: Tree = serde_json::from_str(file.as_str()).unwrap();
-    Some(out)
-}
-
-pub fn serialize_tree(tree: &Tree) -> std::io::Result<()> {
-    let s = serde_json::to_string(tree).unwrap();  
-    fs::write(PATH, s)?;
-    Ok(())
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Tree {   
@@ -34,6 +21,23 @@ impl Tree {
             leds: Vec::new(),
         }
     }
+
+    pub fn serialize(&self) -> std::io::Result<()> {
+        let s = serde_json::to_string(self).unwrap();  
+        fs::write(PATH, s)?;
+        Ok(())
+    }
+
+    pub fn from_file(path: &str) -> Option<Tree> {
+        let file = fs::read_to_string(PATH).unwrap();
+
+        if file == "" {
+            return None
+        }
+
+        let tree: Tree = serde_json::from_str(file.as_str()).unwrap();
+        Some(tree)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -42,3 +46,32 @@ pub struct Colour {
     green: u8,
     blue: u8
 }
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Animation {
+    name: String,
+    len: usize,
+    frames: Vec<Vec<Colour>>,
+}
+
+impl Animation {
+    fn new() -> Animation {
+        Animation {
+            name: "default".into(),
+            len: 0,
+            frames: Vec::new()
+        }
+    }
+
+    fn from_file(path: &str) -> Option<Animation> {
+        let file = fs::read_to_string(PATH).unwrap();
+        if file == "" {
+            return None
+        }
+        let animation: Animation = serde_json::from_str(file.as_str()).unwrap();
+        Some(animation)
+    }
+}
+
+
