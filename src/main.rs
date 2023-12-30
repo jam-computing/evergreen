f1ab593d3f9eed19db1c0a074a6025287ab9b931
Jules White <jules.white1@protonmail.com>
2023-12-30 15:02:02 +0000

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::web::prelude::*;
 pub mod web;
+pub mod tree;
 
 fn main() {
     yew::Renderer::<app::App>::new().render();
--- src/web/app.rs
+++ src/web/app.rs
@@ -1,28 +1,12 @@
-use yew::{function_component, html, Html, use_state };
+use yew::{ function_component, html, Html };
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
     html! {
-        <div>
-            <button {onclick}>{ "+1" } </button>
-            <p> { *counter } </p>
-        </div>
+        <>
+            { "Hello" }
+
+        </>
     }
 
-}
-
-#[function_component(Menu)]
-pub fn menu() -> Html {
-    html! {
-       <> { "Hello, World!" } </> 
-    }
 }
