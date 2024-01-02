a027f7d7ca2f99b4126707505b4c0e49745c2d8f
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-01-02 14:52:57 +0000

    Added some stuff

--- src/tree/animator.rs
+++ src/tree/animator.rs
@@ -1,21 +1,33 @@
 use super::animation::Animation;
 use super::playlist::Playlist;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
+// Th
+
 #[derive(Debug)]
 pub struct Animator {
-    pub playlist: Playlist,
+    pub playlist_arc: Arc<Mutex<Playlist>>,
 }
 
 impl Animator {
-    pub fn new() -> Animator {
-        Animator {
-            playlist: Playlist::new(),
-        }
+    // This returns a new animation struct and starts the associated thread
+    pub fn new(playlist_arc: Arc<Mutex<Playlist>>) -> Animator {
+        let a = Animator {
+            playlist_arc: playlist_arc,
+        };
+        let join_handle = thread::spawn(|| {
+            loop {
+                // Animate stuff
+            }
+        });
+        a
     }
 
-    fn animate() {}
+    fn animate() {
+        Animator::new
+    }
 }
 
 enum AnimatorState {
