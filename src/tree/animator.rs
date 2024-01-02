6cf2c7d626a8d95ab846de79e493db9ae279aa0a
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-01-02 15:48:32 +0000

    Added animator thread management

--- src/tree/animator.rs
+++ src/tree/animator.rs
@@ -1,32 +1,48 @@
 use super::animation::Animation;
 use super::playlist::Playlist;
+use std::sync::atomic::AtomicUsize;
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
-// Th
-
+// The animator class contains all the data
+// associated with the thread that does the animation
+//
+// 'playlist_arc' -> thread safe pointer to the playlist
+// 'thread_join_handle' -> join join_handle of the created thread
+// 'frame_rate' -> thread safe usize which determines the time between frames in ms
 #[derive(Debug)]
 pub struct Animator {
     pub playlist_arc: Arc<Mutex<Playlist>>,
+    pub thread_join_handle: Option<JoinHandle<()>>,
+    pub frame_delay: Arc<Mutex<usize>>,
 }
 
 impl Animator {
     // This returns a new animation struct and starts the associated thread
     pub fn new(playlist_arc: Arc<Mutex<Playlist>>) -> Animator {
         let a = Animator {
-            playlist_arc: playlist_arc,
+            playlist_arc: playlist_arc.clone(),
+            thread_join_handle: None,
+            frame_delay: Arc::new(Mutex::new(15)),
         };
-        let join_handle = thread::spawn(|| {
-            loop {
-                // Animate stuff
-            }
-        });
-        a
-    }
 
-    fn animate() {
-        Animator::new
+        // Variables the thread needs to communicate
+        let frame_delay = a.frame_delay.clone(); // Approx 60 fps
+        let thread_playlist_arc = playlist_arc.clone();
+
+        let join_handle = thread::spawn(move || {
+            let current_playlist = thread_playlist_arc;
+            let frame_delay = frame_delay;
+
+            let frame_delay = loop {
+                thread::sleep(Duration::new(frame_delay.lock().unwrap()));
+
+                // Display the animation on the tree here
+            };
+        });
+
+        a
     }
 }
 
